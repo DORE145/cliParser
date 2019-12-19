268c555db0393de7b410d1c8640759a7001c93a8
Mikhail Kuzhel <dore145@gmail.com>
2019-12-19 17:14:24 +0300

    Refactor parsing

--- internal/filePrser.go
+++ internal/filePrser.go
@@ -10,42 +10,67 @@ import (
 	"github.com/DORE145/cliParser/internal/data"
 )
 
-func Parse(file *os.File, flags int) {
-	scaner := bufio.NewScanner(file)
-	var linesCounter, wordsCounter, symbolsCounter, printableSymbolsCounter int
-	uniqueWords := data.NewWordSet()
-	regExp, regExpErr := regexp.Compile("[[:punct:]]") //Regexp to detect punctuation
+type ParsingResult struct {
+	LinesCounter   int
+	WordsCounter   int
+	SymbolsCounter int
+	UniqueWords    data.WordSet
+}
 
-	if regExpErr != nil {
-		fmt.Printf("Regular expression creation failed: %s", regExpErr)
-		return
-	}
+func Parse(file *os.File, flags int) *ParsingResult {
+	scaner := bufio.NewScanner(file)
+	result := ParsingResult{UniqueWords: data.NewWordSet()}
 
 	for scaner.Scan() {
-		linesCounter++
-		if (flags & 13) > 0 { // flag mask 1101
+		result.LinesCounter++
+		if (flags & 13) > 0 { //Flag mask 1101
 			line := scaner.Text()
-			symbolsCounter += len(line)
+			result.SymbolsCounter += len(line)
 
-			lineWords := strings.Fields(line)
-			wordsCounter += len(lineWords)
-			for _, word := range lineWords {
-				symbolsCounter += len(word)
-				normalizedWord := strings.ToLower(word) //Changing to lower case and removing puncuation
-				normalizedWord = regExp.ReplaceAllString(normalizedWord, "")
-
-				printableSymbolsCounter += len(normalizedWord)
-				uniqueWords.Add(normalizedWord)
+			if (flags & 12) > 0 { //Flag mask 1100
+				getUnique := (flags & 8) > 0
+				lineWords := scanLine(line, getUnique)
+				result.WordsCounter += len(lineWords)
+				if getUnique {
+					result.UniqueWords.AddAll(lineWords)
+				}
 			}
 		}
 	}
 
-	//TODO: Results printer
-	//TODO: Split parse function
 	//TODO: Write tests
+	//TODO: Add exported functions comments
 
 	scannerErr := scaner.Err()
 	if scannerErr != nil {
 		fmt.Printf("Error parsing the file: %s", scannerErr)
 	}
+
+	return &result
+}
+
+func scanLine(line string, getUnique bool) []string {
+	lineWords := strings.Fields(line)
+
+	if !getUnique {
+		return lineWords
+	}
+
+	result := make([]string, len(lineWords))
+
+	regExp, regExpErr := regexp.Compile("[[:punct:]“”]") //Regexp to detect punctuation and non standart quotation marks
+	if regExpErr != nil {
+		fmt.Printf("Regular expression creation failed: %s", regExpErr)
+		return nil
+	}
+
+	for _, word := range lineWords {
+		normalizedWord := strings.ToLower(word) //Changing to lower case and removing puncuation
+		normalizedWord = regExp.ReplaceAllString(normalizedWord, "")
+		if len(normalizedWord) != 0 { //Skiping empty words
+			result = append(result, normalizedWord)
+		}
+	}
+
+	return result
 }
