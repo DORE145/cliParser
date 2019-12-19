325077c5462e472040d997702e2f02874facac4e
Mikhail Kuzhel <dore145@gmail.com>
2019-12-19 17:15:25 +0300

    Add mass words addition

--- internal/data/wordSet.go
+++ internal/data/wordSet.go
@@ -12,6 +12,12 @@ func (set WordSet) Add(word string) {
 	set.words[word] = true
 }
 
+func (set WordSet) AddAll(words []string) {
+	for _, word := range words {
+		set.Add(word)
+	}
+}
+
 func (set WordSet) Remove(word string) {
 	set.words[word] = false
 }
@@ -22,4 +28,15 @@ func (set WordSet) Size() int {
 
 func (set WordSet) Contains(word string) bool {
 	return set.words[word]
+}
+
+func (set WordSet) Words() []string {
+	words := make([]string, len(set.words))
+	i := 0
+	for word := range set.words {
+		words[i] = word
+		i++
+	}
+
+	return words
 }
